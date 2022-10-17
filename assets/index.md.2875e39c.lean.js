60eb7b5b7167fa871cc5e74174f586c294551e98
mino01x <mino01x@users.noreply.github.com>
2022-10-17 06:36:16 +0000

    deploy: 2baaf8c36192c85a9fe8d2fdb909395e0754bca7

---
+++ assets/index.md.2875e39c.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o}from"./app.398e1b98.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{"layout":"home","hero":{"name":"Pokemonon","text":"Collection of Utilities"}},"headers":[],"relativePath":"index.md"}'),n={name:"index.md"};function a(r,s,c,i,d,m){return o(),t("div")}const l=e(n,[["render",a]]);export{_ as __pageData,l as default};
